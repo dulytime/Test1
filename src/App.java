5e84518cd62c51f1a109363930149eb22f106456
dulytime <dulyapap2000@gmail.com>
2023-05-10 12:56:52 +0700

    Francis

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        Scanner a = new Scanner(System.in);
+        System.out.print("Insert number : ");
+        int n = a.nextInt();
+        for (int i=0;i<n;i++) System.out.println("Francis");
     }
 }
